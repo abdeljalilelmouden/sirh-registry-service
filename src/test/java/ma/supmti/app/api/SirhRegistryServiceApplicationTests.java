ea327f7d68b22f28b91ea16a6c18ec8e1263d3c1
ABDELJALIL EL MOUDEN <elmoudene123@gmail.com>
2021-05-07 00:23:49 +0200

    registry service

---
+++ src/test/java/ma/supmti/app/api/SirhRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.supmti.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirhRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
